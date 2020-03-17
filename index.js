d92d3494e12fb6304efcd467a3dd31ceac00b4fe
jagulto <jagulto@extremenetworks.com>
2020-03-16 21:18:47 -0700

    added stuff for migrations

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require('express');
 const app = express();
 const router = require('./routes/main.js');
 
-var port = process.env.PORT;
+var port = process.env.PORT || 8096;
 var http = require('http');
 var server = http. createServer(app);
 
 app.use(express.static('client'));
 app.use('/', router);
 
+if (process.env.NODE_ENV === 'development') {
+    require("dotenv").config();
+}
+
 server.listen(port, function() {
     console.log('Server is running...');
 });
-
