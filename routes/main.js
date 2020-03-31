04895a43e982aa628de06f0710159ece78cfa25e
jagulto <jagulto@extremenetworks.com>
2020-03-30 18:16:42 -0700

    fix

--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const db = require('../db');
 const path = require('path');
 
-router.get('/', (request, resource) => {
-    resource.sendFile(path.join(__dirname + '/index.html'));
+router.get("/", (request, response) => {
+    response.sendFile(path.join(__dirname + '/index.html'));
 });
+
+router.get("/tests", (request, response) => {
+    db.any(`INSERT INTO test_table ("testString") VALUES ('Hello at ${Date.now()}')`)
+        .then(_ => db.any(`SELECT * FROM test_table`))
+        .then(results => response.json(results))
+        .catch(error => {
+            console.log(error)
+            response.json({ error })
+        })
+})
 
 module.exports = router;
