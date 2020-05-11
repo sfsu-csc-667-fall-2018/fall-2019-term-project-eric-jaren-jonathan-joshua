8a335fc3fd13c9a0df2c880b6af68014b9befa1d
Joshua Agulto <jagulto@mail.sfsu.edu>
2020-05-11 16:46:38 -0700

    changed port

--- index_backup.js
+++ index_backup.js
@@ -14,7 +14,7 @@ const formatMessage = require('./models/messages');
 require('./config/passport')(passport);
 
 const app = express();
-const port = process.env.PORT || 8096;
+const port = process.env.PORT;
 const server = http.createServer(app);
 const io = socket(server);
 
