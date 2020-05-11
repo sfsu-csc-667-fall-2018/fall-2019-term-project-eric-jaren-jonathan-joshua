a6c45941ec87defc3770a98bb8f4a18c5f371dc1
Joshua Agulto <jagulto@extremenetworks.com>
2020-05-11 00:16:18 -0700

    finished account management with database. working sign in and sign off and user tracking with express sessions

--- index.js
+++ index.js
@@ -1,38 +1,92 @@
-const express = require('express');
-const http = require('http');
-const router = require('./routes/main.js'); 
-const socket = require('socket.io');
-
-const Sequelize = require('sequelize');
-
-const app = express();
-const port = process.env.PORT || 8096;
-const server = http.createServer(app);
-const io = socket(server);
-
-
-app.use(express.static(' client'));
-app.use('/', router);
-app.use(express.static(__dirname));
-
-io.on('connection', socket => {
-    socket.broadcast.emit('message', 'A new user has joined the chat!');
-
-    socket.on('disconnect', () => {
-        io.emit('message', 'A user has disconnected...');
-    })
-
-    // Listen incoming chats//
-    socket.on('chatMessage', (message) => {
-        io.emit('message', message);
-        console.log(message);
-    })
-});
-
-if (process.env.NODE_ENV === 'development') {
-    require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
 }
 
-server.listen(port, () => {
-    console.log('Server is running...');
-});
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+
+const db = require('./db');
+
+const initializePassport = require('./passport-config')
+
+initializePassport(passport);
+
+const users = [{ id: 7034, username: 'Kristine', password: '$2b$10$TNbWtmWf927cOV2/ybfn..Exys1y0/0uefVvvyI52bOWd2RueEILy'}];
+
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'secret',
+  resave: false,
+  saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+app.use(express.static(__dirname));
+
+app.get('/', checkAuthenticated, (req, res) => {
+  res.render('lobby.ejs', { name: req.user.username })
+})
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
+  res.render('index.ejs')
+})
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
+
+app.get('/signup', checkNotAuthenticated, (req, res) => {
+  res.render('register.ejs')
+})
+
+app.post('/signup', checkNotAuthenticated, async (req, res) => {
+  const hashedPassword = await bcrypt.hash(req.body.password, 10)
+
+  const newUser = {
+    id: Math.floor((Math.random() * 9999) + 1),
+    username: req.body.username,
+    password: hashedPassword
+  }
+  db.any(`INSERT INTO account_db (id, username, password) VALUES (${newUser.id}, '${newUser.username}', '${newUser.password}')`)
+    .then(data => {
+      console.log(newUser);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+
+  res.redirect('/');
+})
+
+app.delete('/logout', (req, res) => {
+  req.logOut()
+  res.redirect('/login')
+})
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+
+  res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/')
+  }
+  next()
+}
+
+app.listen(8096)
--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,26 +1,36 @@
-const socket = io();
-
+// const socket = io();
 const chat = document.getElementById('cr-chat');
+const chatBox = document.getElementById('cr-chat-container');
+const accountDetailsButton = document.getElementById('account-details-button');
+
+// startup //
+window.onload = function() {
+    
+}
+
+// account logged in listener // 
+
 
 // CR-Chat Listener //
 chat.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const message = e.target.elements.message.value;
-
-    socket.emit('chatMessage', message);
+    // socket.emit('chatMessage', message);
 })
 
 // Chat Box Update //
-socket.on('message', message => {
-    updateChat(message);
-})
+// socket.on('message', message => {
+//     console.log(socket.username);
+//     updateChat(message);
+//     chatBox.scrollTop = chatBox.scrollHeight;
+// })
 
 function updateChat(message) {
     const domNode = document.createElement('div');
     domNode.classList.add('cr-chat-message');
-    domNode.innerHTML = '<p class="chat-meta">Joshua</p>' + 
-                        '<p class="chat-text" style="margin: 0px">' + message + '</p>' +
+    domNode.innerHTML = '<p class="chat-meta">' + message.username +'</p>' + 
+                        '<p class="chat-text" style="margin: 0px">' + message.text + '</p>' +
                         '<p class="chat-meta" style="text-align: right">September 12, 2019</p>';
     
     document.querySelector('.cr-chat-box').appendChild(domNode);
