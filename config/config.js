6091538127e67fa3403dcc0bf296b4ffa3ab25a9
Joshua Agulto <jagulto@mail.sfsu.edu>
2020-05-17 01:12:58 -0700

    fixing

--- config/config.js
+++ config/config.js
@@ -1,21 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-<<<<<<< HEAD
-  "development": {
-    "use_env_variable": "DATABASE_URL",
-    "dialect": "postgres"
-  },
-  "tests": {
-    "use_env_variable": "DATABASE_URL",
-    "dialect": "postgres"
-  },
-  "production": {
-    "use_env_variable": "DATABASE_URL",
-    "dialect": "postgres"
-  }
-}
-=======
     "development": {
         "use_env_variable": "DATABASE_URL",
         "dialect": "postgres"
@@ -31,4 +16,3 @@ module.exports = {
         "dialect": "postgres"
     }
 }
->>>>>>> 8e9d81269afc1db386a0b8a10503a4217d92a5fe
