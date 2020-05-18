cb44e010fa906ebdd69f800cf87fdb13c2fc09d2
Joshua Agulto <jagulto@mail.sfsu.edu>
2020-05-17 23:25:01 -0700

    90% done. need to add some behavior and GG

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const initializePassport = require('./passport-config');
 const Game = require('./models/game');
 let gameArray = [];
 
-const port = process.env.PORT;
+const port = process.env.PORT || 8096;
 const server = http.createServer(app);
 const io = socket(server);
 
@@ -71,6 +71,10 @@ io.on('connection', (socket) => {
     socket.emit('userInfo', (user));
   }
 
+  socket.on('disconnect', () => {
+    if (socket.user) socket.broadcast.to(socket.user.lobby).emit('consoleMessage', socket.user.username + ' has left the chat.');
+  })
+
   socket.on('joinRoom', (user) => {
     socket.join(user.lobby, function () { });
     socket.broadcast.to(user.lobby).emit('consoleMessage', user.username + ' has joined the chat.');
@@ -91,27 +95,38 @@ io.on('connection', (socket) => {
           db.any(`INSERT INTO lobby_info VALUES (${user.lobby})`)
             .then(() => {
               db.any(`UPDATE lobby_info SET player_list = array_append(player_list, '${user.id}') WHERE lobby_id = ${user.lobby}`)
-              .then (() => {
-                db.any(`SELECT * FROM lobby_info`)
-                .then(results => {
-                  let gameInfo = results.find(lobby => lobby.lobby_id === parseInt(user.lobby));
-                  io.to(gameInfo.lobby_id).emit('getGameInfo', gameInfo);
+                .then(() => {
+                  db.any(`SELECT * FROM lobby_info`)
+                    .then(results => {
+                      let gameInfo = results.find(lobby => lobby.lobby_id === parseInt(user.lobby));
+                      io.to(gameInfo.lobby_id).emit('getGameInfo', gameInfo);
+                    })
                 })
-              })
             })
         } else {
           db.any(`UPDATE lobby_info SET player_list = array_remove(player_list, ${user.id})`)
             .then(() => {
               db.any(`UPDATE lobby_info SET player_list = array_append(player_list, '${user.id}') WHERE lobby_id = ${user.lobby}`)
-              .then(() => {
-                db.any(`SELECT * FROM lobby_info`)
-                .then(results => {
-                  let gameInfo = results.find(lobby => lobby.lobby_id === parseInt(user.lobby));
-                  io.to(gameInfo.lobby_id).emit('getGameInfo', gameInfo);
+                .then(() => {
+                  db.any(`SELECT * FROM lobby_info`)
+                    .then(results => {
+                      let gameInfo = results.find(lobby => lobby.lobby_id === parseInt(user.lobby));
+                      io.to(gameInfo.lobby_id).emit('getGameInfo', gameInfo);
+                    })
                 })
-              })
             })
         }
+      })
+  })
+
+  socket.on('forceUpdate', (user) => {
+    db.any(`SELECT * FROM lobby_${user.lobby}`)
+      .then(results => {
+        db.any(`SELECT * FROM lobby_info`)
+          .then((results2) => {
+            let lobby = results2.find(lobby => lobby.lobby_id === parseInt(user.lobby));
+            io.to(user.lobby).emit('updateGame', { gameInfo: lobby, deck: results });
+          })
       })
   })
 
@@ -126,13 +141,101 @@ io.on('connection', (socket) => {
       })
   })
 
-  socket.on('disconnect', () => {
-    if (socket.user) socket.broadcast.to(socket.user.lobby).emit('consoleMessage', socket.user.username + ' has left the chat.');
+  socket.on('changeGameState', (lobby) => {
+    db.any(`UPDATE lobby_info SET game_state = ${lobby.game_state}
+    WHERE lobby_id = ${lobby.lobby_id}`)
+      .then(() => {
+        if (lobby.game_state == 0) {
+          db.any(`TRUNCATE TABLE lobby_${lobby.lobby_id}`)
+            .then(() => {
+              db.any(`SELECT * FROM lobby_info`)
+                .then(results => {
+                  let gameInfo = results.find(gameInfo => gameInfo.lobby_id === parseInt(lobby.lobby_id));
+                  io.to(gameInfo.lobby_id).emit('getGameInfo', gameInfo);
+                })
+            })
+        } else {
+          let newDraw = Math.floor((Math.random() * 39) + 0);
+          db.any(`INSERT INTO lobby_${lobby.lobby_id} (card_id, player_id) VALUES (${newDraw}, '0')`)
+            .then(() => {
+              db.any(`UPDATE lobby_info SET turn = ${0} WHERE lobby_id = ${lobby.lobby_id}`)
+                .then(() => {
+                  db.any(`SELECT * FROM lobby_info`)
+                    .then(results => {
+                      let gameInfo = results.find(gameInfo => gameInfo.lobby_id === parseInt(lobby.lobby_id));
+                      for (let i = 0; i < gameInfo.player_list.length; i++) {
+                        for (let j = 0; j < 7; j++) {
+                          newDraw = Math.floor((Math.random() * 49) + 0);
+                          db.any(`INSERT INTO lobby_${lobby.lobby_id} (card_id, player_id) VALUES
+                          (${newDraw}, ${gameInfo.player_list[i]})`)
+                          .then(() => {
+                            io.to(gameInfo.lobby_id).emit('getGameInfo', gameInfo);
+                          })
+                        }
+                      }
+                    })
+                })
+            })
+        }
+      })
   })
+
+  socket.on('drawCard', (user) => {
+    let newDraw = Math.floor((Math.random() * 49) + 0);
+    db.any(`INSERT INTO lobby_${user.lobby} (card_id, player_id) VALUES (${newDraw}, ${user.id})`)
+      .then(() => {
+        db.any(`SELECT * FROM lobby_info`)
+        .then((results) => {
+          let lobby = results.find(lobby => lobby.lobby_id === parseInt(user.lobby))
+          if (lobby.turn == lobby.player_list.length - 1) db.any(`UPDATE lobby_info SET turn = ${0} WHERE lobby_id = ${user.lobby}`)
+          else db.any(`UPDATE lobby_info SET turn = ${lobby.turn + 1} WHERE lobby_id = ${user.lobby}`)
+          db.any(`SELECT * FROM lobby_${user.lobby}`)
+            .then(results => {
+              db.any(`SELECT * FROM lobby_info`)
+                .then((results2) => {
+                  let lobby = results2.find(lobby => lobby.lobby_id === parseInt(user.lobby));
+                  io.to(user.lobby).emit('updateGame', { gameInfo: lobby, deck: results });
+                })
+            })
+        })
+      })
+  })
+
+  socket.on('endTurn', (user) => {
+    db.any(`SELECT * FROM lobby_${user.lobby}`)
+      .then((results) => {
+        let discard = results.find(discard => discard.player_id === 0);
+        if (verifyDiscard(user.discardCard, discard.card_id) || verifyDiscard(discard.card_id, user.discardCard)) {
+          db.any(`DELETE FROM lobby_${user.lobby} WHERE player_id = ${0}`)
+            .then(() => {
+              db.any(`INSERT INTO lobby_${user.lobby} (card_id, player_id) VALUES (${user.discardCard}, ${0})`)
+                .then(() => {
+                  db.any(`DELETE FROM lobby_${user.lobby} WHERE (card_id, player_id) = (${user.discardCard}, ${user.id})`)
+                    .then(() => {
+                      db.any(`SELECT * FROM lobby_info`)
+                        .then((results) => {
+                          let lobby = results.find(lobby => lobby.lobby_id === parseInt(user.lobby))
+                          if (lobby.turn == lobby.player_list.length - 1) db.any(`UPDATE lobby_info SET turn = ${0} WHERE lobby_id = ${user.lobby}`)
+                          else db.any(`UPDATE lobby_info SET turn = ${lobby.turn + 1} WHERE lobby_id = ${user.lobby}`)
+                          db.any(`SELECT * FROM lobby_${user.lobby}`)
+                            .then(results => {
+                              db.any(`SELECT * FROM lobby_info`)
+                                .then((results2) => {
+                                  let lobby = results2.find(lobby => lobby.lobby_id === parseInt(user.lobby));
+                                  io.to(user.lobby).emit('updateGame', { gameInfo: lobby, deck: results });
+                                })
+                            })
+                        })
+                    })
+                })
+            })
+        }
+      })
+  })
+
 
   // Listen incoming chats//
   socket.on('chatMessage', (message) => {
-    console.log(message.lobby);
     io.in(message.lobby).emit('message', message);
   })
 
@@ -237,4 +340,39 @@ function checkNotAuthenticated(req, res, next) {
     return res.redirect('/');
   }
   next();
+}
+
+/*
+
+  GAME FUNCTIONS
+
+*/
+
+function verifyDiscard(card1, card2) {
+  if (card1 == 48 || card1 == 49 || card2 == 48 || card2 == 49) return true;
+
+  a = normalizeCard(card1);
+  b = normalizeCard(card2);
+
+  if (a.value == b.value) return true;
+  else if (a.color == b.color) return true;
+  else return false;
+}
+
+function normalizeCard(card) {
+  let normalizedCard = {
+    color: '',
+    value: ''
+  };
+
+  if (card < 10 || card == 40 || card == 41) { normalizedCard.color = 'Red'; }
+  else if (card < 20 || card == 42 || card == 43) { normalizedCard.color = 'Yellow'; }
+  else if (card < 30 || card == 44 || card == 45) { normalizedCard.color = 'Green'; }
+  else if (card < 40 || card == 46 || card == 47) { normalizedCard.color = 'Blue'; }
+
+  if (card < 40) normalizedCard.value = card % 10;
+  else if (card == 40 || card == 42 || card == 44 || card == 46) normalizedCard.value = 11;
+  else if (card == 41 || card == 43 || card == 45 || card == 47) normalizedCard.value = 12;
+
+  return normalizedCard;
 }
--- resources/scripts/lobby.js
+++ resources/scripts/lobby.js
@@ -5,16 +5,24 @@ const chatBox = document.getElementById('cr-chat-container');
 const startGameScreen = document.getElementById('cr-game-start');
 const joinButton = document.getElementById('cr-game-join');
 const startButton = document.getElementById('cr-game-start-button');
+const endButton = document.getElementById('cr-game-end');
 const leaveButton = document.getElementById('cr-game-leave');
 const playerList = document.getElementById('cr-player-list');
+const playerHand = document.getElementById('cr-game-hand');
+const discardStack = document.getElementById('cr-discard');
+const turnHeader = document.getElementById('cr-game-title');
+const enemyTop = document.getElementById('cr-enemy-top');
+const enemyLeft = document.getElementById('cr-enemy-left');
+const enemyRight = document.getElementById('cr-enemy-right');
 
 let id;
 let user;
-let gameInfo;
+let currentHand;
+let discard;
+let turn;
 
 socket.on('connect', () => {
     id = socket.id;
-    console.log(socket);
 })
 
 socket.on('userInfo', user => {
@@ -24,29 +32,73 @@ socket.on('userInfo', user => {
 socket.on('foundInfo', userInfo => {
     user = userInfo;
     user.lobby = new URLSearchParams(window.location.search).get('Lobby');
-    
+
     socket.emit('joinRoom', user);
 })
 
 socket.on('getGameInfo', gameInfo => {
-    if (gameInfo.player_list) {
-        if (gameInfo.player_list.includes(user.id)) {
-            joinButton.classList.add('cr-hidden');
-            leaveButton.classList.remove('cr-hidden');
-            this.gameInfo = gameInfo;
-        } else {
-            joinButton.classList.remove('cr-hidden');
-            leaveButton.classList.add('cr-hidden');
+    if (gameInfo.game_state == 0) {
+        startGameScreen.classList.remove('cr-hidden');
+        if (gameInfo.player_list) {
+            if (gameInfo.player_list.includes(user.id)) {
+                joinButton.classList.add('cr-hidden');
+                leaveButton.classList.remove('cr-hidden');
+                this.gameInfo = gameInfo;
+            } else {
+                joinButton.classList.remove('cr-hidden');
+                leaveButton.classList.add('cr-hidden');
+            }
+
+            playerList.innerHTML = '';
+            for (let i = 0; i < gameInfo.player_list.length; i++) {
+                playerList.innerHTML += '<h2>' + gameInfo.player_list[i] + '</h2>';
+            }
         }
 
-        playerList.innerHTML = '';
-        for (let i = 0; i < gameInfo.player_list.length; i++) {
-            playerList.innerHTML += '<h2>' + gameInfo.player_list[i] + '</h2>';
-        }
+        if (gameInfo.player_list[0] == user.id && gameInfo.player_list.length > 1) startButton.classList.remove('cr-hidden');
+        else startButton.classList.add('cr-hidden');
+    } else if (gameInfo.game_state == 1) {
+        startGameScreen.classList.add('cr-hidden');
+        socket.emit('forceUpdate', user);
+    }
+})
+
+socket.on('updateGame', (gameInfo) => {
+    let deck = gameInfo.deck;
+    currentHand = [];
+
+    for (let i = 0; i < deck.length; i++) {
+        if (deck[i].player_id == user.id) currentHand.push(deck[i].card_id);
+        else if (deck[i].player_id == 0) discard = deck[i].card_id;
     }
 
-    if (gameInfo.player_list[0] == user.id && gameInfo.player_list.length > 1) startButton.classList.remove('cr-hidden');
-    else startButton.classList.add('cr-hidden');
+    playerHand.innerHTML = '';
+    for (let i = 0; i < currentHand.length; i++) findCardByID(playerHand, currentHand[i], 'cr-card-inhand');
+
+    discardStack.innerHTML = '';
+    findCardByID(discardStack, discard, '');
+
+    if (gameInfo.gameInfo.player_list[gameInfo.gameInfo.turn] == user.id) {
+        turnHeader.innerHTML = 'Your Turn';
+        turn = true;
+    } else {
+        turnHeader.innerHTML = gameInfo.gameInfo.player_list[gameInfo.gameInfo.turn] + '\'s Turn'
+        turn = false;
+    }
+
+    if (gameInfo.gameInfo.player_list.length == 2) {
+        enemyLeft.classList.add('cr-hidden');
+        enemyRight.classList.add('cr-hidden');
+        enemyTop.classList.remove('cr-hidden');
+    } else if (gameInfo.gameInfo.player_list.length == 3) {
+        enemyLeft.classList.remove('cr-hidden');
+        enemyRight.classList.remove('cr-hidden');
+        enemyTop.classList.add('cr-hidden');
+    } else if (gameInfo.gameInfo.player_list.length == 4) {
+        enemyLeft.classList.remove('cr-hidden');
+        enemyRight.classList.remove('cr-hidden');
+        enemyTop.classList.remove('cr-hidden');
+    }
 })
 
 // CR-Chat Listener //
@@ -62,7 +114,7 @@ chat.addEventListener('submit', (e) => {
         };
 
         chat.reset();
-    
+
         socket.emit('chatMessage', message);
     }
 })
@@ -70,27 +122,8 @@ chat.addEventListener('submit', (e) => {
 // draw listener
 draw.addEventListener('submit', (e) => {
     e.preventDefault();
-    let randomColor = Math.floor((Math.random() * 5) + 1);
-    console.log(randomColor);
-    const domNode = document.createElement('div');
-    switch (randomColor) {
-        case 1: 
-            domNode.innerHTML = '<div class="cr-card-inhand cr-card-red cr-card-front">' + Math.floor((Math.random() * 9) + 1); + '</div>';
-            break;
-        case 2: 
-            domNode.innerHTML = '<div class="cr-card-inhand cr-card-yellow cr-card-front">' + Math.floor((Math.random() * 9) + 1); + '</div>';
-            break;
-        case 3: 
-            domNode.innerHTML = '<div class="cr-card-inhand cr-card-green cr-card-front">' + Math.floor((Math.random() * 9) + 1); + '</div>';
-            break;
-        case 4: 
-            domNode.innerHTML = '<div class="cr-card-inhand cr-card-blue cr-card-front">' + Math.floor((Math.random() * 9) + 1); + '</div>';
-            break;
-        case 5:
-            domNode.innerHTML = '<div class="cr-card-inhand cr-card-rainbow cr-card-front">' + Math.floor((Math.random() * 9) + 1); + '</div>';
-            break;
-    }
-    document.querySelector('.cr-game-hand').appendChild(domNode);
+
+    if (turn) socket.emit('drawCard', user);
 })
 
 // join game listener
@@ -105,13 +138,47 @@ leaveButton.addEventListener('submit', (e) => {
     socket.emit('leaveGame', user);
 })
 
+startButton.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const lobby = {
+        lobby_id: user.lobby,
+        game_state: 1
+    }
+    socket.emit('changeGameState', lobby);
+})
+
+endButton.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const lobby = {
+        lobby_id: user.lobby,
+        game_state: 0
+    }
+
+    socket.emit('changeGameState', lobby);
+})
+
+playerHand.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (turn) {
+        let cardId;
+        if (e.target.id) {
+            cardId = e.target.id;
+        } else if (e.target.tagName == 'IMG' || e.target.tagName == 'P') {
+            cardId = e.target.parentNode.id;
+        }
+
+        user.discardCard = cardId;
+        socket.emit('endTurn', user);
+    }
+})
 
 socket.on('message', message => {
     const domNode = document.createElement('div');
     domNode.classList.add('cr-chat-message');
     domNode.innerHTML = '<p class="cr-chat-name">' + message.username + ': </p><p class="cr-chat-message-content">' + message.text + '</p>';
-                        //'<p class="chat-meta" style="text-align: right">' + message.time + '</p>';
-    
+    //'<p class="chat-meta" style="text-align: right">' + message.time + '</p>';
+
     document.querySelector('.cr-chat-box').appendChild(domNode);
     chatBox.scrollTop = chatBox.scrollHeight;
 })
@@ -119,8 +186,65 @@ socket.on('message', message => {
 socket.on('consoleMessage', message => {
     const domNode = document.createElement('div');
     domNode.innerHTML = '<p class="console-text">' + message + '</p>';
-    
+
     document.querySelector('.cr-chat-box').appendChild(domNode);
 
     chatBox.scrollTop = chatBox.scrollHeight;
 })
+
+function findCardByID(domNode, id, addClass) {
+    if (id < 10) {
+        if (id != 0) domNode.innerHTML += '<div id="' + id + '" class="' + addClass + ' cr-card-red cr-card-front">' + id + '</div>'
+        else domNode.innerHTML += '<div id="' + id + '" class="' + addClass + ' cr-card-red cr-card-front">' +
+            '<img class="cr-card-block" src="resources/images/block-48px.svg" alt="Block"></div>';
+    } else if (id < 20) {
+        if (id != 0) domNode.innerHTML += '<div id="' + id + '" class="' + addClass + ' cr-card-yellow cr-card-front">' + (id - 10) + '</div>'
+        else domNode.innerHTML += '<div id="' + id + '" class="' + addClass + ' cr-card-yellow cr-card-front">' +
+            '<img class="cr-card-block" src="resources/images/block-48px.svg" alt="Block"></div>';
+    } else if (id < 30) {
+        if (id != 0) domNode.innerHTML += '<div id="' + id + '" class="' + addClass + ' cr-card-green cr-card-front">' + (id - 20) + '</div>'
+        else domNode.innerHTML += '<div id="' + id + '" class="' + addClass + ' cr-card-green cr-card-front">' +
+            '<img class="cr-card-block" src="resources/images/block-48px.svg" alt="Block"></div>';
+    } else if (id < 40) {
+        if (id != 0) domNode.innerHTML += '<div id="' + id + '" class="' + addClass + ' cr-card-blue cr-card-front">' + (id - 30) + '</div>'
+        else domNode.innerHTML += '<div id="' + id + '" class="' + addClass + ' cr-card-blue cr-card-front">' +
+            '<img class="cr-card-block" src="resources/images/block-48px.svg" alt="Block"></div>';
+    } else if (id < 50) {
+        switch (id) {
+            case 40:
+                domNode.innerHTML += '<div id="' + id + '" class="' + addClass + ' cr-card-red cr-card-front"><p class="cr-card-draw-2">+2</p></div>';
+                break;
+            case 41:
+                domNode.innerHTML += '<div id="' + id + '" class="' + addClass + ' cr-card-red cr-card-front">' +
+                    '<img class="cr-card-block" src="resources/images/loop-48px.svg" alt="Loop"></div>';
+                break;
+            case 42:
+                domNode.innerHTML += '<div id="' + id + '" class="' + addClass + ' cr-card-yellow cr-card-front"><p class="cr-card-draw-2">+2</p></div>';
+                break;
+            case 43:
+                domNode.innerHTML += '<div id="' + id + '" class="' + addClass + ' cr-card-yellow cr-card-front">' +
+                    '<img class="cr-card-block" src="resources/images/loop-48px.svg" alt="Loop"></div>';
+                break;
+            case 44:
+                domNode.innerHTML += '<div id="' + id + '" class="' + addClass + ' cr-card-green cr-card-front"><p class="cr-card-draw-2">+2</p></div>';
+                break;
+            case 45:
+                domNode.innerHTML += '<div id="' + id + '" class="' + addClass + ' cr-card-green cr-card-front">' +
+                    '<img class="cr-card-block" src="resources/images/loop-48px.svg" alt="Loop"></div>';
+                break;
+            case 46:
+                domNode.innerHTML += '<div id="' + id + '" class="' + addClass + ' cr-card-blue cr-card-front"><p class="cr-card-draw-2">+2</p></div>';
+                break;
+            case 47:
+                domNode.innerHTML += '<div id="' + id + '" class="' + addClass + ' cr-card-blue cr-card-front">' +
+                    '<img class="cr-card-block" src="resources/images/loop-48px.svg" alt="Loop"></div>';
+                break;
+            case 48:
+                domNode.innerHTML += '<div id="' + id + '" class="' + addClass + ' cr-card-rainbow cr-card-front"></div>';
+                break;
+            case 49:
+                domNode.innerHTML += '<div id="' + id + '" class="' + addClass + ' cr-card-rainbow cr-card-front"><p class="cr-card-draw-2">+4</p></div>';
+                break;
+        }
+    }
+}
